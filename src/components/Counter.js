e14cdd97e6b777607e89736e6994581b5cf3bb9c
chihebchabou <chihebchaboutech@gmail.com>
2021-09-01 12:16:15 +0100

    redux with connect

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { decrement, increment } from '../actions/counterAction';
 
-const Counter = () => {
-  return <div></div>;
+const Counter = ({ counter, inc, dec }) => {
+  return (
+    <div>
+      <button
+        onClick={() => {
+          inc();
+        }}
+      >
+        +
+      </button>
+      {counter}
+      <button
+        onClick={() => {
+          dec();
+        }}
+      >
+        -
+      </button>
+    </div>
+  );
 };
 
-export default Counter;
+const mapStateToProps = (state) => ({
+  counter: state.counter,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  inc: () => dispatch(increment()),
+  dec: () => dispatch(decrement()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
---
+++ src/reducers/counterReducer.js
@@ -0,0 +1,24 @@
+import { DECREMENT, INCREMENT } from '../actions/types';
+
+const initialState = {
+  counter: 0,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+
+    case DECREMENT:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+
+    default:
+      return state;
+  }
+};
